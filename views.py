6d8a0b5feeabc36f7ba16ab801d020fc24b8a096
moovlaze <max.moovlaze@gmail.com>
2025-06-30 12:05:26 +0300

    update file main and create views

---
+++ views.py
@@ -0,0 +1,70 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+from schemas import Note, NotesList, NoteID
+from chiper import get_note_id
+
+router = APIRouter()
+
+templates = Jinja2Templates("templates")
+notes_list = NotesList()
+
+def is_secret_exists(data: Note) -> bool:
+	for note in notes_list.all_notes:
+		if note.secret == data.secret:
+			return True
+	return False
+
+			
+@router.get("/", response_class=HTMLResponse)
+async def get_home_page(request: Request):
+	return templates.TemplateResponse(
+		request=request,
+		name="index.html",
+		context={
+			"notes_count": len(notes_list.all_notes),
+			"page_title" : "Записки"
+		}
+	)
+
+@router.post("/create_note")
+async def create_note(data: Note):
+
+	if is_secret_exists(data):
+		return {"response": "faild", "msg": "write another secret phrase"}
+
+	note_id = get_note_id(text=data.text, salt=data.secret)
+	data.note_hash = note_id
+	notes_list.all_notes.append(data)
+	print(notes_list.all_notes)
+	return {"response": "ok", "note_id": note_id}
+
+@router.get('/result/{note_id}', response_class=HTMLResponse)
+async def get_note(request: Request,note_id: str):
+	return templates.TemplateResponse(
+		request=request,
+		name="hash_storage.html",
+		context={
+			"note_id": note_id
+		}
+	)
+
+@router.post('/get_note')
+async def get_note(data: NoteID):
+	for note in notes_list.all_notes:
+		if (note.note_hash == data.note_id) and (note.secret == data.note_secret):
+			note_index = notes_list.all_notes.index(note)
+			notes_list.all_notes.pop(note_index)
+			return {"response": "ok", "note_final_text": note.text}
+	return {"response": "faild", "note_final_text": "Such a note does not exist"}
+		
+@router.get('/note_page/{note_text}', response_class=HTMLResponse)
+async def get_note_page(request: Request, note_text: str):
+	return templates.TemplateResponse(
+		request=request,
+		name="note_page.html",
+		context={
+			"note_text": note_text
+		}
+	)
